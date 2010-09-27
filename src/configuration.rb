1c3669a7325849ae5f4e999175bac1532bfd00ab
Nicholas Henry <nicholas.henry@gmail.com>
2010-09-27 15:55:19 -0400

    Updates to use HTML5 tags

--- src/configuration.rb
+++ src/configuration.rb
@@ -2,9 +2,13 @@ require "rubygems"
 require "bundler/setup"
 require 'compass'
 
-configuration.sass_options = { 
+configuration.sass_options = {
   :load_paths => 
     [File.join(File.expand_path(File.dirname(__FILE__)), 'src', 'stylesheets' )] +
     Compass::Frameworks::ALL.map{|f| f.stylesheets_directory },
   :style => :compressed
+}
+
+configuration.haml_options = {
+  :format => :html5
 }
