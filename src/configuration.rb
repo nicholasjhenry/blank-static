b7ce028697a03e0a5114bffacc0b1ee8d2647712
Nicholas Henry <nicholas.henry@gmail.com>
2011-01-24 13:40:49 -0500

    Use explicit HTML5 option in layout

--- src/configuration.rb
+++ src/configuration.rb
@@ -5,10 +5,5 @@ require 'compass'
 configuration.sass_options = {
   :load_paths => 
     [File.join(File.expand_path(File.dirname(__FILE__)), 'src', 'stylesheets' )] +
-    Compass::Frameworks::ALL.map{|f| f.stylesheets_directory },
-  :style => :compressed
-}
-
-configuration.haml_options = {
-  :format => :html5
+    Compass::Frameworks::ALL.map{|f| f.stylesheets_directory }
 }
